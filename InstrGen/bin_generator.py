772c3e7e6e212192ae0b863ebbfb2ec3eddc90da
Xiao <115740205+xiaoz119@users.noreply.github.com>
2025-05-14 17:59:50 -0400

    clean the code gen

--- InstrGen/bin_generator.py
+++ InstrGen/bin_generator.py
@@ -80,44 +80,49 @@ def i2c_write_axi(axi_base_addr = 0x000, i2c_addr = 0x000, reg_addr = -1, tx_siz
     return instr
 
 
+# ------------------------------------  Generation Preparation  ------------------------------------
 
 CONFIG_LENGTH = 50 # Number of instructions in the configuration
+AXI_BASE_ADDR = 0x000 # Base address of the AXI IIC peripheral
 
-
-bl_check_i2c_read = i2c_read_axi(axi_base_addr=0x000, i2c_addr=0x013, mem_addr=0x000, reg_addr=-1, rx_size=5)
-bl_check_i2c_wrtie = i2c_write_axi(axi_base_addr=0x000,
-                                    i2c_addr=0x013, 
-                                    reg_addr=-1, 
-                                    tx_size=5, 
-                                    buffer=[0x0a, 0x00, 0x00, 0x00, 0x00])
-config_interupt = axi_write_i(axi_wr_address=0x040, data_20_bit=0x00a)
 delay_2ms =   generate_delay_instruction(200000)
 delay_200ms = generate_delay_instruction(20000000)
 nop = generate_nop_instruction()
 interrupt_check = generate_interrupt_instruction()
 
-bendlab_iic_device_check = bl_check_i2c_wrtie + [delay_2ms] + bl_check_i2c_read + [delay_2ms]
+# AXI-I2C IP Interrupt configuration (Trun off)
+config_interupt = axi_write_i(SOFTR, 0x00a)
+
+# Configaration instructions
+# Bendlab Device check
+bl_check_i2c_read = i2c_read_axi(axi_base_addr=AXI_BASE_ADDR, i2c_addr=0x013, mem_addr=0x000, reg_addr=-1, rx_size=5)
+bl_check_i2c_wrtie = i2c_write_axi(axi_base_addr=AXI_BASE_ADDR,
+                                    i2c_addr=0x013, 
+                                    reg_addr=-1, 
+                                    tx_size=5, 
+                                    buffer=[0x0a, 0x00, 0x00, 0x00, 0x00])
+
+bendlab_i2c_device_check = bl_check_i2c_wrtie + [delay_2ms] + bl_check_i2c_read + [delay_2ms]
 
 
-
-
-bl_data_i2c_start = i2c_write_axi(axi_base_addr=0x000, 
+# I2C Device comunication start and data read
+bl_data_i2c_start = i2c_write_axi(axi_base_addr=AXI_BASE_ADDR, 
                                   i2c_addr=0x013, 
                                   reg_addr=-1, 
                                   tx_size=5, 
                                   buffer=[0x00, 0x01, 0x00, 0x00, 0x00])
 
-bl_data_i2c_read = i2c_read_axi(axi_base_addr=0x000, i2c_addr=0x013, mem_addr=0x005, reg_addr=-1, rx_size=5)
+bl_data_i2c_read = i2c_read_axi(axi_base_addr=AXI_BASE_ADDR, i2c_addr=0x013, mem_addr=0x005, reg_addr=-1, rx_size=5)
 
-# ---------------------------  Code Generation ---------------------------
+
+# ------------------------------------  Code Generation ------------------------------------
 # # Data Read with 200ms Delay
 # output_filename = "bendlab_data_read.mem"
-# instr_config = [nop, config_interupt, delay_2ms] + bendlab_iic_device_check + bl_data_i2c_start
+# instr_config = [nop, config_interupt, delay_2ms] + bendlab_i2c_device_check + bl_data_i2c_start
 # instr_exec = [delay_200ms] + bl_data_i2c_read
 
-
 output_filename = "bendlab_data_read_interrupt.mem"
-instr_config = [nop, config_interupt, delay_2ms] + bendlab_iic_device_check + bl_data_i2c_start + [delay_2ms]
+instr_config = [nop, config_interupt, delay_2ms] + bendlab_i2c_device_check + bl_data_i2c_start + [delay_2ms]
 instr_exec = [interrupt_check] + bl_data_i2c_read
 
 print("Config Instruction length: ", len(instr_config), "/" , CONFIG_LENGTH)
@@ -125,50 +130,3 @@ print("Execution Instruction length: ", len(instr_exec))
 # Write the instructions to the file
 create_instruction_file(output_filename, instr_config, instr_exec, addr_config=CONFIG_LENGTH)
 print(f"Instruction file '{output_filename}' created successfully.")
-
-# instr_iic_read = [
-#         axi_write_i(axi_wr_address=0x100, data_20_bit=0x002), # Reset TX FIFO
-#         axi_write_i(axi_wr_address=0x100, data_20_bit=0x001), # Enable IIC
-#         generate_delay_instruction(200000), # Delay for 2m
-#         axi_write_i(axi_wr_address=0x108, data_20_bit=0x127), # Set Slave IIC Address
-#         axi_write_i(axi_wr_address=0x108, data_20_bit=0x205), # start comunication
-
-#         axi_read_compare(axi_address=0x104, compare_value=0x040, compare_operation=0b01), 
-#         axi_read_r(axi_rd_address=0x10c, local_wr_address=0x00, count=1), # Read RX FIFO, write to local address 0x00
-#         axi_read_compare(axi_address=0x104, compare_value=0x040, compare_operation=0b01),
-#         axi_read_r(axi_rd_address=0x10c, local_wr_address=0x01, count=1), # Read RX FIFO, write to local address 0x01
-#         axi_read_compare(axi_address=0x104, compare_value=0x040, compare_operation=0b01),
-#         axi_read_r(axi_rd_address=0x10c, local_wr_address=0x02, count=1), # Read RX FIFO, write to local address 0x02
-#         axi_read_compare(axi_address=0x104, compare_value=0x040, compare_operation=0b01),
-#         axi_read_r(axi_rd_address=0x10c, local_wr_address=0x03, count=1), # Read RX FIFO, write to local address 0x03
-#         axi_read_compare(axi_address=0x104, compare_value=0x040, compare_operation=0b01),
-#         axi_read_r(axi_rd_address=0x10c, local_wr_address=0x04, count=1) # Read RX FIFO, write to local address 0x04
-# ]
-
-# instr_iic_write =[
-#         axi_write_i(axi_wr_address=0x100, data_20_bit=0x002), # Reset TX FIFO
-#         axi_write_i(axi_wr_address=0x100, data_20_bit=0x001), # Enable IIC
-#         generate_delay_instruction(200000), # Delay for 2m
-        
-#         axi_write_i(axi_wr_address=0x108, data_20_bit=0x126), # Set Slave IIC Address
-#         # axi_read_r(axi_rd_address=0x104, local_wr_address=0x00, count=1), # Read SR, write to local address 0x00
-
-#         axi_write_i(axi_wr_address=0x108, data_20_bit=0x00a), # write 0
-#         # axi_read_r(axi_rd_address=0x104, local_wr_address=0x00, count=1), # Read SR, write to local address 0x00
-#         axi_write_i(axi_wr_address=0x108, data_20_bit=0x000), # write 1
-#         # axi_read_r(axi_rd_address=0x104, local_wr_address=0x00, count=1), # Read SR, write to local address 0x00
-#         axi_write_i(axi_wr_address=0x108, data_20_bit=0x000), # write 2
-#         # axi_read_r(axi_rd_address=0x104, local_wr_address=0x00, count=1), # Read SR, write to local address 0x00
-#         axi_write_i(axi_wr_address=0x108, data_20_bit=0x000), # write 3
-#         # axi_read_r(axi_rd_address=0x104, local_wr_address=0x00, count=1), # Read SR, write to local address 0x00
-#         axi_write_i(axi_wr_address=0x108, data_20_bit=0x200),  # write 4      
-# ]
-
-# #  Compare the i2c_read and instr_iic_read
-# for index, (i, j) in enumerate(zip(bl_check_i2c_read, instr_iic_read)):
-#     if i != j:
-#         print(f"Mismatch at index {index}: {i} != {j}")
-
-# for index, (i, j) in enumerate(zip(bl_check_i2c_wrtie, instr_iic_write)):
-#     if i != j:
-#         print(f"Mismatch at index {index}: {i} != {j}")
